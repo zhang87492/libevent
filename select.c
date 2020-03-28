004107bd5cb9cc9fe0cfd5945a3b8789f050fe99
zhang87492 <zhang87492@163.com>
2020-03-28 14:34:45 +0800

    1. 添加select的注释说明

--- select.c
+++ select.c
@@ -67,11 +67,12 @@ extern struct event_list eventqueue;
 extern volatile sig_atomic_t evsignal_caught;
 
 struct selectop {
+	/* 最大的文件句柄。*/
 	int event_fds;		/* Highest fd in fd set */
-	int event_fdsz;
-	fd_set *event_readset;
-	fd_set *event_writeset;
-	sigset_t evsigmask;
+	int event_fdsz;		// 存储信号集需要的字节大小。
+	fd_set *event_readset;/* 读信号事件集合*/
+	fd_set *event_writeset;/* 写信号事件集合*/
+	sigset_t evsigmask;// linux 信号集。用于检测是否有信号发送过来。
 } sop;
 
 void *select_init	(void);
@@ -115,16 +116,22 @@ select_recalc(void *arg, int max)
 	fd_set *readset, *writeset;
 	struct event *ev;
 	int fdsz;
-
+	/*
+	设置event_fds为最大值
+	*/
 	if (sop->event_fds < max)
 		sop->event_fds = max;
-
+	/*
+	如果event_fds不存在，则遍历队列，取最大值。
+	*/
 	if (!sop->event_fds) {
 		TAILQ_FOREACH(ev, &eventqueue, ev_next)
 			if (ev->ev_fd > sop->event_fds)
 				sop->event_fds = ev->ev_fd;
 	}
-
+	/*
+	计算存储event_fds所需要的位数。
+	*/
 	fdsz = howmany(sop->event_fds + 1, NFDBITS) * sizeof(fd_mask);
 	if (fdsz > sop->event_fdsz) {
 		if ((readset = realloc(sop->event_readset, fdsz)) == NULL) {
@@ -151,8 +158,11 @@ select_recalc(void *arg, int max)
 	return (evsignal_recalc(&sop->evsigmask));
 }
 
+/*
+下发任务，发起一次，select。等待信号发送。
+*/
 int
-select_dispatch(void *arg, struct timeval *tv)
+select_dispatch(void *arg, struct timeval *tv)	
 {
 	int maxfd, res;
 	struct event *ev, *next;
@@ -160,20 +170,31 @@ select_dispatch(void *arg, struct timeval *tv)
 
 	memset(sop->event_readset, 0, sop->event_fdsz);
 	memset(sop->event_writeset, 0, sop->event_fdsz);
-
+	/*
+	再初始化，信号集。
+	*/
 	TAILQ_FOREACH(ev, &eventqueue, ev_next) {
 		if (ev->ev_events & EV_WRITE)
 			FD_SET(ev->ev_fd, sop->event_writeset);
 		if (ev->ev_events & EV_READ)
 			FD_SET(ev->ev_fd, sop->event_readset);
 	}
-
+	/*
+	先注册信号。
+	*/
 	if (evsignal_deliver(&sop->evsigmask) == -1)
 		return (-1);
-
+	/*
+	调用select函数，等待事件发送。
+	tv如果有值，则超时自动返回。
+	*/
 	res = select(sop->event_fds + 1, sop->event_readset, 
 	    sop->event_writeset, NULL, tv);
-
+	/*
+	select函数完后。需再重新注册信号事件。
+	如果select中有事件发生，需要再重新注册一遍事件。或者select中，是事件触发select结束
+	会导致程序未处理接下来的信号事件
+	*/
 	if (evsignal_recalc(&sop->evsigmask) == -1)
 		return (-1);
 
@@ -182,14 +203,18 @@ select_dispatch(void *arg, struct timeval *tv)
 			log_error("select");
 			return (-1);
 		}
-
+		/*
+		处理信号。
+		*/
 		evsignal_process();
 		return (0);
 	} else if (evsignal_caught)
 		evsignal_process();
 
 	LOG_DBG((LOG_MISC, 80, "%s: select reports %d", __func__, res));
-
+	/*
+	监听读事件，或者写事件，是否有事件发生。
+	*/
 	maxfd = 0;
 	for (ev = TAILQ_FIRST(&eventqueue); ev != NULL; ev = next) {
 		next = TAILQ_NEXT(ev, ev_next);
@@ -214,6 +239,12 @@ select_dispatch(void *arg, struct timeval *tv)
 	return (0);
 }
 
+/*
+添加事件。
+如果只是select的信号事件，就增加到信号事件集里。
+如果不是select的信号事件。就增加最大的文件fd
+这样，自然就添加到了select的监听事件集里。
+*/
 int
 select_add(void *arg, struct event *ev)
 {
@@ -234,8 +265,9 @@ select_add(void *arg, struct event *ev)
 
 /*
  * Nothing to be done here.
+ 文件句柄，扩大了就没有再缩小了。
+ 删除信号事件。
  */
-
 int
 select_del(void *arg, struct event *ev)
 {
